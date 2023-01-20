1937179f6123c64156a77795a1da1ca24b8d8ff0
RaixsG <jhardelloficial@gmail.com>
2023-01-20 12:49:58 -0500

    fix: mejora de estilos css

--- src/components/counter/styles.js
+++ src/components/counter/styles.js
@@ -1,13 +1,18 @@
 import styled from "styled-components"
 
 export const CounterContainer = styled.div`
-    background-color: #ffc107;
+
+    width: 60%;
+    aspect-ratio: 4/2;
+    backdrop-filter: blur(30px);
     padding: 15px;
     border-radius: 5px;
     font-family: sans-serif;
-    box-shadow: 0 4px 12px #333;
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
     h1 {
         margin-bottom: 10px;
+        color: white;
+        font-size: 12vw;
     }
 
     button {
@@ -18,12 +23,16 @@ export const CounterContainer = styled.div`
         :active {
             transform: scale(.96);
         }
+        font-size: 3vw;
+        width: 15%;
+        color: #444;
+        aspect-ratio: 1;
         transition: transform .2s;
         border: none;
-        padding: 5px 8px;
-        border-radius: 7px;
-        margin: 0 .5rem;
-        box-shadow: 0 3px 6px #333;
+        padding: 5px;
+        border-radius: 50%;
+        margin: 0 1.5rem;
+        box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
     }
     
 `
